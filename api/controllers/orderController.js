0edf2279279703b61e36374ee2eb3a3247110ac1
Jacinta <jacdubby@yahoo.com>
2018-09-12 23:56:07 +0100

    ft(update-status-order): update orders status API endpoint
    User should be able to update orders status
    
    [Finishes #160374280]

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -9,12 +9,12 @@ const allOrders = (request, response) => {
 };
 
 const getOrderId = (request, response) => {
-const getId = orders.find(item => item.orderId === parseInt(request.params.id, 10));
-if (!getId) return response.status(404).send('The order with the given ID is not found');
-return response.status(200).json({
-  getId,
-  message: 'Get a specific order is successful'
-});
+  const getId = orders.find(item => item.orderId === parseInt(request.params.id, 10));
+  if (!getId) return response.status(404).send('The order with the given ID is not found');
+  return response.status(200).json({
+    getId,
+    message: 'Get a specific order is successful'
+  });
 };
 
 const postOrders = (request, response) => {
@@ -35,4 +35,21 @@ const postOrders = (request, response) => {
   });
 };
 
-export default { allOrders, getOrderId, postOrders };
+const updateStatus = (request, response) => {
+  const orderStatus = orders.find(item => item.orderId === parseInt(request.params.id, 10));
+  if (!orderStatus) return response.status(404).send('The status with the given order ID was not found');
+  const id = orders.indexOf(orderStatus);
+  orderStatus.status = request.body.status;
+  orders[id] = orderStatus;
+  return response.status(200).json({
+    orderStatus,
+    message: 'Status updated successfully'
+  });
+};
+
+export default {
+  allOrders,
+  getOrderId,
+  postOrders,
+  updateStatus
+};
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/orders', orderController.allOrders);
 router.get('/orders/:id', orderController.getOrderId);
 router.post('/orders', orderController.postOrders);
+router.put('/orders/:id', orderController.updateStatus);
 
 
 export default router;
