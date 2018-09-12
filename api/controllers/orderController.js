0a1c8cd804ed01f18a85280b796de548301be305
Jacinta <jacdubby@yahoo.com>
2018-09-12 22:29:19 +0100

    ft(get-order-id): get a specific order API endpoint
    User should be able to get a specific order
    
    [Finishes #160374195]

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -2,10 +2,19 @@ import orders from '../models/orders';
 
 const allOrders = (request, response) => {
   if (!orders) return response.status(404).send('No order was found');
-  return response.json({
+  return response.status(200).json({
     orders,
     message: 'Get all orders is successful'
   });
 };
 
-export default { allOrders };
+const getOrderId = (request, response) => {
+const getId = orders.find(item => item.orderId === parseInt(request.params.id, 10));
+if (!getId) return response.status(404).send('The order with the given ID is not found');
+return response.status(200).json({
+  getId,
+  message: 'Get a specific order is successful'
+});
+};
+
+export default { allOrders, getOrderId };
