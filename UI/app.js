c3518ec545f47d56f5123db5fb5955269e44c053
Jacinta <jacdubby@yahoo.com>
2018-09-12 14:49:12 +0100

    ft(ui-template): create add button in admin dashboard
    Admin will be able to add food items to menu
    
    [Finishes #160373488]

--- UI/app.js
+++ UI/app.js
@@ -1,22 +1,22 @@
 const modal = document.getElementById('modalPage');
 const closeBtn = document.querySelector('.close-btn');
-const modalBtn = document.querySelector('.modalBtn');
-//  for (let i = 0; i < modalBtn.length; i++ ) {
-//   modalBtn[i].addEventListener('click', function() {
-//      modal.style.display = 'block';
-//    })
-//  }
+const modalBtn = document.querySelectorAll('.modalBtn');
+ for (let i = 0; i < modalBtn.length; i++ ) {
+  modalBtn[i].addEventListener('click', function() {
+     modal.style.display = 'block';
+   })
+ }
 
-modalBtn.onclick = () => {
-modal.style.display = 'block';
-}
+// modalBtn.addEventListener('click', function() {
+// modal.style.display = 'block';
+// });
  
 closeBtn.onclick = () => {
   modal.style.display = 'none';
 }
 
 window.onclick = (event) => {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
